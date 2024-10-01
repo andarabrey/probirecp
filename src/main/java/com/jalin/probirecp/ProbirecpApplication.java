13cbb963715556967b3a5ba0a87b14a2c0fe1825
breygas.andara <andarabrey@gmail.com>
2024-10-01 14:31:01 +0700

    first commit

---
+++ src/main/java/com/jalin/probirecp/ProbirecpApplication.java
@@ -0,0 +1,13 @@
+package com.jalin.probirecp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbirecpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbirecpApplication.class, args);
+	}
+	//test
+}
---
+++ src/test/java/com/jalin/probirecp/ProbirecpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalin.probirecp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbirecpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
